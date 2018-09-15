bf70e51126d270983b65119f04be3700d53c2202
Matt McAlister <matt.mcalister93@gmail.com>
2018-09-15 17:22:00 -0400

    finalized progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,54 +1,23 @@
 import React from "react"
 import { connect } from "react-redux"
 
-const Original = (props) => {
-	const percentage = "100%"//`${props.hours/props.goal*100}%`
-	return (
-		<div id="progress-container">
-			<h1>{percentage} complete</h1>
-			<div id="full-progress-bar">
-				<div id="progress-completed" style={{width: percentage}}>
-					<p id="progress-hours">{props.hours}h</p>
-				</div>
-				<p>{props.goal}h</p>
-			</div>
-		</div>)
-}
-
-const SecondTry = (props) => {
-	const percentage = 80//`${props.hours/props.goal*100}%`
-	return (
-		<div id="progress-container">
-			<h1>{percentage}% complete</h1>
-			<div id="full-progress-bar">
-				<div className="progress-bar-cap" id="begin-cap"/>
-				<div id="rect-progress-bar">
-					<div id="progress-completed" style={{width: percentage*3.25}}/>
-					<p id="progress-hours">{props.hours}h</p>
-				</div>
-				<p className="progress-bar-cap" id="goal">{props.goal}h</p>
-			</div>
-		</div>)
-}
 
 const ProgressBar = (props) => {
-	// circle (behind)
-	// rectangle
-	// inner Rectangle
-	// inner circle, position relative moved over
-	// circle (in front)
-
-
-	// it needs to be two bars that are position absolute or relative, one placed ontop of the other
-	// maybe the second one is rendered directly below, but position relative moved above it
 	if (props.hours) {
-		const percentage = .6//props.hours/props.goal
+		const percentage = props.hours/props.goal
+		let progressBarCont = {
+			gridTemplateColumns: `minmax(12vh, ${percentage*100}%) auto minmax(5%, 12vh)`
+		}
 		return (
-			<div id="progress-container">
-				<h1>{percentage*100}% complete</h1>
-			<div id="background-progress-bar" className="bar">100h</div>
-		<div id="progress-completed-layer" className="bar" style={{width: `${percentage*90}%`}}>100h</div>
-
+		<div id="progress-container">
+			<h1>{percentage*100}% complete</h1>
+			<div id="progress-bar-container" style={progressBarCont}>
+				<div id="progress-so-far">
+					<div id="progress-label-circle">{props.hours}h</div>
+				</div>
+				<div id="progress-separator" />
+				<div id="progress-remainder">{props.goal}h</div>
+			</div>
 		</div>)
 	} else {
 		return null
