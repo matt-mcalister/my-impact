ca30a2f9cf0f8530b752db315a3a29f68bd018ae
matt-mcalister <matt.mcalister93@gmail.com>
2018-09-19 09:05:40 -0400

    eventShow now holds host and attendee info in state

--- src/components/EventShow.js
+++ src/components/EventShow.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Modal } from "semantic-ui-react"
 import { connect } from "react-redux"
 import { removeSelectedEvent } from "../actions"
+import { firebase } from "../firebase"
 
 const defaultState = {
   host: null,
@@ -11,7 +12,36 @@ class EventShow extends React.Component {
   state = defaultState
 
   componentDidMount(){
+    if (this.props.selectedEvent) {
+      this.setHost()
+    }
+  }
 
+  componentDidUpdate(prevProps){
+    if (this.props.selectedEvent && (!prevProps.selectedEvent || this.props.selectedEvent.id !== prevProps.selectedEvent.id) ){
+      this.setHost()
+      this.setAttendees()
+    }
+  }
+
+  setHost = () => {
+    firebase.db.collection("participant").doc(this.props.selectedEvent.hostId)
+      .get().then( doc => this.setState({host: doc.data()}) )
+  }
+
+  setAttendees = () => {
+    const attendees = this.props.selectedEvent.attendingParticipantIds
+    for (let uid in attendees) {
+      if (attendees[uid]) {
+        this.addAttendeeToState(uid)
+      }
+    }
+  }
+
+  addAttendeeToState(uid){
+    const attendees = this.state.attendees || []
+    firebase.db.collection("participant").doc(this.props.selectedEvent.hostId)
+      .get().then( doc => this.setState({attendees: [...attendees, doc.data()]} ) )
   }
 
   handleClose = () => {
@@ -20,7 +50,7 @@ class EventShow extends React.Component {
   }
 
   render() {
-    console.log(this.props);
+    console.log(this.state);
     return (
       <Modal id="selected-event" open={!!this.props.selectedEvent} onClose={this.handleClose}>
         <div id="selected-event-img-cont" className="img-container-centered">
@@ -28,7 +58,7 @@ class EventShow extends React.Component {
         </div>
         <h1>{this.props.selectedEvent && this.props.selectedEvent.title}</h1>
         <div id="selected-event-sub-header">
-          <h4>Posted by {this.state.host ? this.state.host : "anonymous"}</h4>
+          <h4>Posted by {this.state.host ? this.state.host.name : "anonymous"}</h4>
           <h4>{this.props.selectedEvent && (new Date(this.props.selectedEvent.dateStart).toLocaleDateString('en-us-iso8601', {month: 'short', day: "numeric", year: "numeric"}))}</h4>
           <h4>{this.props.selectedEvent && this.props.selectedEvent.location.address}</h4>
         </div>
